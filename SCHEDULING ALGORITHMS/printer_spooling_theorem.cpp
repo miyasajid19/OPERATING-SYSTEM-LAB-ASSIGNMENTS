0490476e6514f17f92528a1ada4384b9a61edfcc
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-09 20:07:22 +0530

    printer spooling theorem

--- SCHEDULING ALGORITHMS/printer_spooling_theorem.cpp
+++ SCHEDULING ALGORITHMS/printer_spooling_theorem.cpp
@@ -3,20 +3,20 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
-
-std::queue<int> printQueue;  // Queue to hold print jobs
-std::mutex mtx;              // Mutex for critical section
-std::condition_variable cv;  // Condition variable for synchronization
+using namespace std;
+queue<int> printQueue;  // Queue to hold print jobs
+mutex mtx;              // Mutex for critical section
+condition_variable cv;  // Condition variable for synchronization
 
 void printer(int job) {
     // Simulate printing a job
-    std::this_thread::sleep_for(std::chrono::seconds(2));  // Simulate time taken for printing
-    std::cout << "Job " << job << " printed." << std::endl;
+    this_thread::sleep_for(chrono::seconds(2));  // Simulate time taken for printing
+    cout << "Job " << job << " printed." << endl;
 }
 
 void printJob() {
     while (true) {
-        std::unique_lock<std::mutex> lock(mtx);
+        unique_lock<mutex> lock(mtx);
 
         // Wait until there is a job in the queue
         cv.wait(lock, [](){ return !printQueue.empty(); });
@@ -33,11 +33,11 @@ void printJob() {
 }
 
 void addJob(int job) {
-    std::unique_lock<std::mutex> lock(mtx);
+    unique_lock<mutex> lock(mtx);
 
     // Add job to the print queue
     printQueue.push(job);
-    std::cout << "Job " << job << " added to the queue." << std::endl;
+    cout << "Job " << job << " added to the queue." << endl;
 
     lock.unlock();  // Release lock for other threads
 
@@ -46,20 +46,20 @@ void addJob(int job) {
 }
 
 int main() {
-    std::thread printerThread(printJob);  // Start the printer thread
+    thread printerThread(printJob);  // Start the printer thread
 
     // Add some print jobs to the queue
-    std::this_thread::sleep_for(std::chrono::seconds(1)); // Simulate a small delay before adding jobs
+    this_thread::sleep_for(chrono::seconds(1)); // Simulate a small delay before adding jobs
     addJob(1);
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    this_thread::sleep_for(chrono::seconds(1));
     addJob(2);
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    this_thread::sleep_for(chrono::seconds(1));
     addJob(3);
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    this_thread::sleep_for(chrono::seconds(1));
     addJob(4);
 
     // Wait for the printer thread to process jobs (in real-world applications, this would be more dynamic)
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    this_thread::sleep_for(chrono::seconds(10));
 
     printerThread.join();  // Wait for the printer thread to finish
 

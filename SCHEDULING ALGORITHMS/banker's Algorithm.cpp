66944523384180827b39cd12bf748edc4b45fd3f
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-25 21:55:15 +0530

    banker's Algorithm

--- SCHEDULING ALGORITHMS/banker's Algorithm.cpp
+++ SCHEDULING ALGORITHMS/banker's Algorithm.cpp
@@ -2,30 +2,39 @@
 #include <vector>
 using namespace std;
 
-bool isSafeState(vector<vector<int>>& alloc, vector<vector<int>>& max, vector<vector<int>>& avail, int P, int R, vector<int>& safeSequence) {
+bool isSafeState(vector<vector<int>> &alloc, vector<vector<int>> &max, vector<vector<int>> &avail, int P, int R, vector<int> &safeSequence)
+{
     vector<int> work(R);
     vector<bool> finish(P, false);
-    for (int i = 0; i < R; i++) {
+    for (int i = 0; i < R; i++)
+    {
         work[i] = avail[0][i]; // Initially, work = available
     }
 
     int count = 0;
-    while (count < P) {
+    while (count < P)
+    {
         bool found = false;
-        for (int i = 0; i < P; i++) {
-            if (!finish[i]) {
+        for (int i = 0; i < P; i++)
+        {
+            if (!finish[i])
+            {
                 bool canAllocate = true;
                 // Check if process i can finish
-                for (int j = 0; j < R; j++) {
-                    if (max[i][j] - alloc[i][j] > work[j]) {
+                for (int j = 0; j < R; j++)
+                {
+                    if (max[i][j] - alloc[i][j] > work[j])
+                    {
                         canAllocate = false;
                         break;
                     }
                 }
 
-                if (canAllocate) {
+                if (canAllocate)
+                {
                     // If process i can finish, release its resources
-                    for (int j = 0; j < R; j++) {
+                    for (int j = 0; j < R; j++)
+                    {
                         work[j] += alloc[i][j];
                     }
                     finish[i] = true;
@@ -35,14 +44,16 @@ bool isSafeState(vector<vector<int>>& alloc, vector<vector<int>>& max, vector<ve
                 }
             }
         }
-        if (!found) {
+        if (!found)
+        {
             return false; // If no process can proceed, system is in a deadlock state
         }
     }
     return true; // If all processes can finish, it's a safe state
 }
 
-int main() {
+int main()
+{
     int P, R;
     cout << "Enter number of processes and resources: ";
     cin >> P >> R;
@@ -50,35 +61,45 @@ int main() {
     vector<vector<int>> alloc(P, vector<int>(R)), max(P, vector<int>(R)), avail(1, vector<int>(R));
 
     cout << "Enter the Allocation Matrix: \n";
-    for (int i = 0; i < P; i++) {
-        for (int j = 0; j < R; j++) {
+    for (int i = 0; i < P; i++)
+    {
+        for (int j = 0; j < R; j++)
+        {
             cin >> alloc[i][j];
         }
     }
 
     cout << "Enter the Maximum Matrix: \n";
-    for (int i = 0; i < P; i++) {
-        for (int j = 0; j < R; j++) {
+    for (int i = 0; i < P; i++)
+    {
+        for (int j = 0; j < R; j++)
+        {
             cin >> max[i][j];
         }
     }
 
     cout << "Enter the Available Resources: \n";
-    for (int i = 0; i < R; i++) {
+    for (int i = 0; i < R; i++)
+    {
         cin >> avail[0][i];
     }
 
     vector<int> safeSequence; // To store the safe sequence of processes
 
-    if (isSafeState(alloc, max, avail, P, R, safeSequence)) {
+    if (isSafeState(alloc, max, avail, P, R, safeSequence))
+    {
         cout << "System is in a safe state.\n";
         cout << "Safe Sequence: ";
-        for (int i = 0; i < safeSequence.size(); i++) {
+        for (int i = 0; i < safeSequence.size(); i++)
+        {
             cout << "P" << safeSequence[i];
-            if (i != safeSequence.size() - 1) cout << " -> ";
+            if (i != safeSequence.size() - 1)
+                cout << " -> ";
         }
         cout << endl;
-    } else {
+    }
+    else
+    {
         cout << "System is in a deadlock state.\n";
     }
 

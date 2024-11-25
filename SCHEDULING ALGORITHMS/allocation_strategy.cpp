54af029218f22467269bd7ffe86b58a928e35ff7
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-11-25 20:18:54 +0530

    temp

---
+++ SCHEDULING ALGORITHMS/allocation_strategy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cstdlib>
+#include <vector>
+using namespace std;
+class AllocationStartegy{
+
+};
+int main()
+{
+    vector<int> memoryBlock={10,20,30,40,50,60,70,80,90,100};
+    vector<int>processSize={5,10,15,25,35,45,55,65,75,85,95};
+
+    return EXIT_SUCCESS;
+}
